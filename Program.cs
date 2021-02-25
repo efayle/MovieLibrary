70573ccf07eff1368812a0b8259004b795db96e7
Elena Fayle <efayle@my.wctc.edu>
2021-02-25 12:15:00 -0600

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NLog.Web;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MovieLibraryAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog(path).GetCurrentClassLogger();
+
+            logger.Info("Program Started");
+
+            logger.Info("Program Ended");
+        }
+    }
+}
