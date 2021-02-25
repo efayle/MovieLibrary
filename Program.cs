839bb43958943f5987290b27dcaaff7473a37dd4
Elena Fayle <efayle@my.wctc.edu>
2021-02-25 12:19:18 -0600

    Movie data from file to memory

--- Program.cs
+++ Program.cs
@@ -15,6 +15,26 @@ namespace MovieLibraryAssignment
 
             logger.Info("Program Started");
 
+            var file = "movies.csv";
+
+            List<int> movieID = new List<int>();
+            List<string> movieTitle = new List<string>();
+            List<string> movieGenre = new List<string>();
+
+            StreamReader sr = new StreamReader(file);
+            sr.ReadLine();
+            // load movie data from file into memory
+            while(!sr.EndOfStream) {
+                string line = sr.ReadLine();
+                int startingPosition = line.IndexOf('"');
+                string[] array = line.Split(',');
+
+                movieID.Add(Convert.ToInt32(array[0]));
+                movieTitle.Add(array[1]);
+                movieGenre.Add(array[2].Replace("|",", "));
+            }
+            sr.Close();
+
             logger.Info("Program Ended");
         }
     }
